08dca20e5ef2044c8b1a01a42ae8feda8de96ea4
Randolfo Machado <talktorandolph@live.com>
2019-09-28 10:04:17 -0300

    Other entities and second migration

---
+++ SallesWebMVC/Models/Seller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SallesWebMVC.Models
+{
+    public class Seller
+    {
+        //Atributes
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        //Constructors
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        //Methods
+        public void AddSaleRecord(SalesRecord salesRecord)
+        {
+            SalesRecords.Add(salesRecord);
+        }
+
+        public void RemoveSaleRecord(SalesRecord salesRecord)
+        {
+            SalesRecords.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
