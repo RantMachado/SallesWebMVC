582d1a2270132d93e6e8ebd7726f356aed39a914
Randolfo Machado <talktorandolph@live.com>
2019-09-28 22:20:10 -0300

    SellersControler

--- SallesWebMVC/Data/SallesWebMVCContext.cs
+++ SallesWebMVC/Data/SallesWebMVCContext.cs
@@ -8,13 +8,12 @@ namespace SallesWebMVC.Models
 {
     public class SallesWebMVCContext : DbContext
     {
-        public SallesWebMVCContext (DbContextOptions<SallesWebMVCContext> options)
-            : base(options)
-        {
-        }
-
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
+
+        public SallesWebMVCContext (DbContextOptions<SallesWebMVCContext> options) : base(options)
+        {
+        }
     }
 }
