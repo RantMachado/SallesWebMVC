3ebd5f454f9a7eabb44239cdb35af6623ff700fd
Randolfo.Gama <talktorandolph@live.com>
2019-10-23 00:39:35 -0300

    SellerFormViewModel and Department select component

---
+++ SallesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using SallesWebMVC.Models;
+
+namespace SallesWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly SallesWebMVCContext _context;
+
+        public DepartmentService(SallesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
--- SallesWebMVC/Services/SellerService.cs
+++ SallesWebMVC/Services/SellerService.cs
@@ -22,7 +22,6 @@ namespace SallesWebMVC.Services
 
         public void Insert(Seller seller)
         {
-            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
