667f893093c8c74fb77cc585865dacb4060a39f2
Randolfo.Gama <talktorandolph@live.com>
2019-10-26 19:44:11 -0300

    Exception handling  for delete (referencial integrity)

---
+++ SallesWebMVC/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SallesWebMVC.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
